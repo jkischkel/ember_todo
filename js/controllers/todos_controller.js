424b3b75112e2f56a61c9bffe12e618d30692486
jkischkel <jk@soundcloud.com>
2014-02-04 23:04:28 +0100

    Add more behaviour

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -16,8 +16,33 @@ Todos.TodosController = Ember.ArrayController.extend({
 
       // Save the new model
       todo.save();
+    },
+
+    clearCompleted: function() {
+      var completed = this.filterBy('isCompleted', true);
+      completed.invoke('deleteRecord');
+      completed.invoke('save');
     }
   },
+
+  hasCompleted: function() {
+    return this.get('completed') > 0;
+  }.property('completed'),
+ 
+  allAreDone: function(key, value) {
+    if (value === undefined) {
+      return !!this.get('length') && this.everyProperty('isCompleted', true);
+    
+    } else {
+      this.setEach('isCompleted', value);
+      this.invoke('save');
+      return value;
+    }
+  }.property('@each.isCompleted'),
+
+  completed: function() {
+    return this.filterBy('isCompleted', true).get('length');
+  }.property('@each.isCompleted'),
 
   remaining: function() {
     return this.filterBy('isCompleted', false).get('length');
--- js/router.js
+++ js/router.js
@@ -1,9 +1,45 @@
 Todos.Router.map(function() {
-  this.resource('todos', { path: '/' });
+  this.resource('todos', { path: '/' }, function() {
+    this.route('all');
+    this.route('active');
+    this.route('completed');
+  });
 });
 
 Todos.TodosRoute = Ember.Route.extend({
   model: function() {
     return this.store.find('todo');
+  }
+});
+
+Todos.TodosIndexRoute = Ember.Route.extend({
+  model: function() {
+    return this.modelFor('todos');
+  }
+});
+
+// /completed
+Todos.TodosActiveRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.filter('todo', function(todo) {
+      return !todo.get('isCompleted');
+    });
+  },
+
+  renderTemplate: function(controller) {
+    this.render('todos/index', { controller: controller })
+  }
+});
+
+// /completed
+Todos.TodosCompletedRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.filter('todo', function(todo) {
+      return todo.get('isCompleted');
+    });
+  },
+
+  renderTemplate: function(controller) {
+    this.render('todos/index', { controller: controller });
   }
 });
